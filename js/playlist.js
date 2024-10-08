dcdd936b5829ccf3dfaa0b5af954b1546006a48c
bondarin87 <110008441+bondarin87@users.noreply.github.com>
2024-10-08 11:38:31 -0500

    Add files via upload

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "Крылатые качели",
+    artist: "&#127826; &#127755; &#127861; &#9749; &#129728; &#129504; &#129716;",
+    img: "img1",
+    audio: "music3"
+  },
+  {
+    name: "Нежность",
+    artist: "2000",
+    img: "img1",
+    audio: "music2"
+  },
+  {
+    name: "Прекрасное далеко",
+    artist: "2000",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "Нежность2",
+    artist: "2000",
+    img: "img1",
+    audio: "music4"
+  }
+]
